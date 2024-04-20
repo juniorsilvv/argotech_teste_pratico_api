df42a50ba0a6a9bc7a812f2ab72d52857b5de7ce
Junior Silva <hjuniorsilva@outlook.com>
2024-04-19 22:19:27 -0300

    feat: docs

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,9 @@ Route::prefix('api')
 ->middleware(AuthMiddleware::class)
 ->controller(TodoListController::class)
 ->group(function(){
-    Route::get('/all', 'all');
-    Route::post('/create', 'create');
-    Route::put('/update/{id}', 'update');
-    Route::put('update/status/{id}', 'updateStatus');
-    Route::delete('/delete/{id}', 'delete');
+    Route::get('/all', 'all'); // Lista todas as tarefas
+    Route::post('/create', 'create'); // Cria uma tarefa
+    Route::put('/update/{id}', 'update'); // Atualiza uma tarefa
+    Route::put('update/status/{id}', 'updateStatus'); // Muda o status de uma tarefa
+    Route::delete('/delete/{id}', 'delete'); // Apaga uma tarefa
 });
