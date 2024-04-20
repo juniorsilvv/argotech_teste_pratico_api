7e1b5788ea2470635c9aba512082e8918a45d97a
Junior Silva <hjuniorsilva@outlook.com>
2024-04-19 22:16:05 -0300

    feat: adicionando middleware nas rotas

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoListController;
+use App\Http\Middleware\AuthMiddleware;
 
-Route::prefix('api')->controller(TodoListController::class)->group(function(){
+Route::prefix('api')
+->middleware(AuthMiddleware::class)
+->controller(TodoListController::class)
+->group(function(){
     Route::get('/all', 'all');
     Route::post('/create', 'create');
     Route::put('/update/{id}', 'update');
